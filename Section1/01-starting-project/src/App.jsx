ef3160057331833c432fe3fca65ce004427abfdc
an-Unusually-Large-Cat <leo@innovit.co.za>
2024-07-04 19:21:15 +0200

    Dynamic Values

--- Section1/01-starting-project/src/App.jsx
+++ Section1/01-starting-project/src/App.jsx
@@ -1,16 +1,38 @@
-function App() {
-  return (
-    <div>
+import reactImage from './assets/react-core-concepts.png'
+
+
+const reactDescriptions=['Fundemental','crucial','core']
+function genRandomInt(max)
+{
+  return Math.floor(Math.random()*(max))
+}
+
+
+function Header()
+{
+  const random=genRandomInt(reactDescriptions.length)
+  const value =reactDescriptions[random]
+  console.log(random)
+  
+  // console.log('working')
+  return(
       <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <img src={reactImage} alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
-          Fundamental React concepts you will need for almost any app you are
+          {value} React concepts you will need for almost any app you are
           going to build!
         </p>
       </header>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
       <main>
-        <h2>Time to get started!</h2>
+
       </main>
     </div>
   );
