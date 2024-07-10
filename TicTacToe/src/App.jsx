f816515f2c8ee7b321f0f9e487e70a515ed7a1fb
an-Unusually-Large-Cat <leo@innovit.co.za>
2024-07-10 07:23:21 +0200

    added end screen

--- TicTacToe/src/App.jsx
+++ TicTacToe/src/App.jsx
@@ -3,7 +3,10 @@ import Log from "./components/log";
 
 import Player from "./components/Player"
 import Gameboard from "./components/Gameboard"
+import GameOver from "./components/GameOver";
 import { WINNING_COMBINATIONS } from "./winning-combinations";
+
+const initialGameBoard=[[null,null,null],[null,null,null],[null,null,null]]
 
 function getActivePlayer(gameTurns)
 {
@@ -18,8 +21,41 @@ function getActivePlayer(gameTurns)
 function App() {
   // const [activePlayer,setActivePlayer]=React.useState('X');
   const [gameTurns,setGameTurns]=React.useState([])
+  let gameBoard= initialGameBoard;
+
+  
   
   const activePlayer=getActivePlayer(gameTurns)
+  for(const turn of gameTurns){
+    const {square,player}=turn
+    const {row,col}=square
+    // console.log(square)
+    // console.log(row)
+    // console.log(col)
+    gameBoard[row][col]=player
+  }
+
+  let winner=null;
+  
+  for (const combination of WINNING_COMBINATIONS)
+  {
+    const first = gameBoard[combination[0].row][combination[0].column]
+    const second = gameBoard[combination[1].row][combination[1].column]
+    const third =gameBoard[combination[2].row][combination[2].column]
+    // console.log(first)
+    // console.log(second)
+    // console.log(third)
+    if(
+      first&&
+      first===second&&
+      first===third
+    ){
+      winner=first
+      console.log(winner)
+    }
+
+  }
+  const hasDraw= gameTurns.length ===9 && !winner
 
   function handleSelectSquare(rowIndex,colIndex)
   {
@@ -43,9 +79,10 @@ function App() {
 
       
     </ol>
+    {(winner|| hasDraw) && <GameOver winner={winner}/>}
     <Gameboard 
     onSelectSquare={handleSelectSquare} 
-    turns ={gameTurns}
+    board ={gameBoard}
     />
     </div>
     <Log turns={gameTurns}/>
--- TicTacToe/src/components/Gameboard.jsx
+++ TicTacToe/src/components/Gameboard.jsx
@@ -1,38 +1,13 @@
 
 import React from "react"
 
-const initialGameBoard=[[null,null,null],[null,null,null],[null,null,null]]
 
-export default function Gameboard({onSelectSquare,turns})
+export default function Gameboard({onSelectSquare,board})
 {
-    // console.log(turns)
-    let gameBoard= initialGameBoard;
-    // console.log(gameBoard)
-    for(const turn of turns){
-        const {square,player}=turn
-        const {row,col}=square
-        // console.log(square)
-        // console.log(row)
-        // console.log(col)
-        gameBoard[row][col]=player
-    }
-    // const [gameBoard,setGameBoard]= React.useState(initialGameBoard)
-    // function handleSelectSquare(row,col)
-    // {
-        
-
-    //     setGameBoard((prevGameBoard)=>
-    //     {
-    //         const newArr= [...prevGameBoard.map(innerArr=>[...innerArr])]
-    //         newArr[row][col]=activePlayerSymbol
-    //         return newArr
-    //     });
-    //     onSelectSquare();
-    // }
-
+  
 
     return <ol id='game-board'>
-        {gameBoard.map((row,rowIndex)=> <li key={rowIndex}>
+        {board.map((row,rowIndex)=> <li key={rowIndex}>
             <ol>
                 {row.map((playerSymbol,colIndex)=><li key={colIndex}>
                     <button 
